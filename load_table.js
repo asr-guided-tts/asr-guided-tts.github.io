e7db21bc7126bca20972d35b6042434e10083550
IceCreamWW <wangwei.sjtu@sjtu.edu.cn>
2024-10-05 15:46:16 +0800

    minor fix

--- load_table.js
+++ load_table.js
@@ -9,7 +9,7 @@ function createAudioHTML(path, flat) {
       ' type="audio/wav">Your browser does not support the audio element.</audio>';
 }
 
-function generate_number_of_asr_models_table(tableId, uttids, page) {
+function generate_guidance_models_table(tableId, uttids, page) {
   let numPerPage = 4;
   let table = document.getElementById(tableId);
   let nrRows = table.rows.length;
@@ -130,19 +130,19 @@ function generate_target_speech_length_table(tableId, uttids, page) {
 
 
 
-uttids_for_number_of_asr_models = ["1089_134691_000039_000001", "121_121726_000020_000001", "1320_122612_000035_000003", "1580_141083_000010_000002", "1580_141083_000018_000000", "1580_141084_000068_000000", "1995_1826_000030_000002", "1995_1836_000007_000001", "3570_5694_000011_000001", "3570_5695_000002_000004", "4446_2271_000007_000004"]
+uttids_for_guidance_models = ["1089_134691_000039_000001", "121_121726_000020_000001", "1320_122612_000035_000003", "1580_141083_000010_000002", "1580_141083_000018_000000", "1580_141084_000068_000000", "1995_1826_000030_000002", "1995_1836_000007_000001", "3570_5694_000011_000001", "3570_5695_000002_000004", "4446_2271_000007_000004"]
 uttids_for_target_speech_length = ["1089_134691_000039_000001", "121_121726_000020_000001", "1320_122612_000035_000003", "1580_141083_000010_000002", "1580_141083_000018_000000", "1580_141084_000068_000000", "1995_1826_000030_000002", "1995_1836_000007_000001", "3570_5694_000011_000001", "3570_5695_000002_000004", "4446_2271_000007_000004"]
 
-generate_number_of_asr_models_table('number_of_asr_models', uttids_for_number_of_asr_models, 1);
+generate_guidance_models_table('guidance_models', uttids_for_guidance_models, 1);
 generate_target_speech_length_table('target_speech_length', uttids_for_target_speech_length, 1);
 
 $(document).ready(function() {
   for (let i = 1; i <= 3; i++) {
-    let id = '#number_of_asr_models-' + i;
+    let id = '#guidance_models-' + i;
     $(id).click(function() {
-      generate_number_of_asr_models_table(
-          'number_of_asr_models',
-          uttids_for_number_of_asr_models, i);
+      generate_guidance_models_table(
+          'guidance_models',
+          uttids_for_guidance_models, i);
       $(id).parent().siblings().removeClass('active');
       $(id).parent().addClass('active');
       return false;
